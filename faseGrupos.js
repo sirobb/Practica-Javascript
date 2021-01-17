f41b4dc046cf33c777d6fd14d8334ef6c2505c69
boris alarcon <boris.alarco@gmail.com>
2021-01-17 13:27:21 -0300

    Version de Prueba17/01/2021

--- faseGrupos.js
+++ faseGrupos.js
@@ -1,6 +1,21 @@
 const equipos = ["Chile", "Senegal", "Peru", "Islandia", "Austria", "Honduras", "Argentina", "Bosnia", "Colombia", "Venezuela", "Portugal", "Francia",
-    "Holanda","Iran","Dinamarca","Arabia Saudita","Corea Del Sur","Marruecos", "España","Camerun","Nigeria","Japon","Panama", "Inglaterra","Brasil", "Italia", "EEUU","Egipto", "Canada","Rusia", "Uruguay", "Serbia"]
+    "Holanda", "Iran", "Dinamarca", "Arabia Saudita", "Corea Del Sur", "Marruecos", "España", "Camerun", "Nigeria", "Japon", "Panama", "Inglaterra", "Brasil", "Italia", "EEUU", "Egipto", "Canada", "Rusia", "Uruguay", "Serbia"]
+// const grupoA = new Grupo(), grupoB = new Grupo()
+//     , grupoC = new Grupo(), grupoD = new Grupo()
+//     , grupoE = new Grupo(), grupoF = new Grupo()
+//     , grupoG = new Grupo(), grupoH = new Grupo()
+    
 console.log(equipos.length)
-equipos.forEach(i => {
-    console.log(i)
-});
+for (let i = [], i=0; i < equipos.length; i++) a[i]=i 
+   
+function shuffle(array) {
+    let tmp, current, top = array.length;
+    if (top) while(--top) {
+        current = Math.floor(Math.random() * (top+1));
+        tmp = array[current]
+        array[current] = array[top];
+        array[top]= tmp
+    }
+    return array
+}
+a= shuffle(equipos)
