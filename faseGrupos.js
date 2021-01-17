1730964092ba241b14a76562c4ebc60ef6e6c5dc
boris alarcon <boris.alarco@gmail.com>
2021-01-16 21:31:01 -0300

    creacion 32 equipos Fase de grupos

---
+++ faseGrupos.js
@@ -0,0 +1,6 @@
+const equipos = ["Chile", "Senegal", "Peru", "Islandia", "Austria", "Honduras", "Argentina", "Bosnia", "Colombia", "Venezuela", "Portugal", "Francia",
+    "Holanda","Iran","Dinamarca","Arabia Saudita","Corea Del Sur","Marruecos", "España","Camerun","Nigeria","Japon","Panama", "Inglaterra","Brasil", "Italia", "EEUU","Egipto", "Canada","Rusia", "Uruguay", "Serbia"]
+console.log(equipos.length)
+equipos.forEach(i => {
+    console.log(i)
+});
