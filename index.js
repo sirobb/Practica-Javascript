f7bab41eeb3ce9bc000402fbdc9e66170924ea0b
boris alarcon <boris.alarco@gmail.com>
2021-01-16 21:12:51 -0300

    primera version de prueba terminada

--- index.js
+++ index.js
@@ -1,26 +1,137 @@
-const GrupoA = ["Chile", "Brasil"]
-const GrupoB = ["Argentina", "Mexico"]
-const GrupoC = ["Uruguay", "Venezuela"]
-const GrupoD = ["Camerun", "Egipto"]
-const GrupoE = ["Etiopia", "Inglaterra"]
-const GrupoF = ["España", "Holanda"]
-const GrupoG = ["Italia", "Francia"]
-const GrupoH = ["Portugal", "Jamaica"]
-const Eliminatorias = [GrupoA, GrupoB, GrupoC, GrupoD, GrupoE, GrupoF, GrupoG, GrupoH];
-const Numero1 = Math.floor(Math.random() * 10);
-const Numero2 = Math.floor(Math.random() * 10);
+function Grupo(equipo1, equipo2) {
+    this.equipo1 = equipo1
+    this.equipo2 = equipo2
+}
 
-function Partido(equipo1, equipo2) {
-    equipo1 = GrupoA[0] + Numero1;
-    equipo2 = GrupoB[0] + Numero2;
-    console.log(`${equipo1}-${equipo2}`)
-    if (equipo1 > equipo2) {
-        console.log(`Gana ${equipo1}`)
-    }
-    else if(equipo1 < equipo2) {
-        console.log(`Gana ${equipo2}`)
+const grupoA = new Grupo("Chile", "Peru"), grupoB = new Grupo("Austria", "Argentina")
+    , grupoC = new Grupo("Colombia", "Venezuela"), grupoD = new Grupo("Portugal", "Francia")
+    , grupoE = new Grupo("Holanda", "España"), grupoF = new Grupo("Inglaterra", "Italia")
+    , grupoG = new Grupo("Egipto", "Rusia"), grupoH = new Grupo("Uruguay", "Serbia")
+const faseEliminatorias = ["Chile", "Peru", "Austria", "Argentina", "Colombia", "Venezuela", "Portugal", "Francia",
+    "Holanda", "España", "Inglaterra", "Italia", "Egipto", "Rusia", "Uruguay", "Serbia"]
+const cuartosDeFinal = []
+const semiFinales = []
+const tercerYcuarto = []
+const final = []
+
+const generaScore1 = function () {
+    const min = 0, max = 10
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+const generaScore2 = function () {
+    const min = 0, max = 10
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+function jugarOctavos(equipo1, score1, equipo2, score2) {
+
+    if (score1 > score2) {
+        cuartosDeFinal.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    } else if (score2 > score1) {
+
+        cuartosDeFinal.push(equipo2)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo2}`)
+    } else if (score1 === score2) {
+
+        cuartosDeFinal.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
     }
 }
-console.log(`Equipos Eliminatorias => ${Eliminatorias}`);
-console.log("--OCTAVOS DE FINAL!!---")
-Partido()
+function jugar4tos(equipo1, score1, equipo2, score2) {
+
+    if (score1 > score2) {
+        semiFinales.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    } else if (score2 > score1) {
+
+        semiFinales.push(equipo2)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo2}`)
+    } else if (score1 === score2) {
+
+        semiFinales.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    }
+}
+function jugarsemifinal(equipo1, score1, equipo2, score2) {
+
+    if (score1 > score2) {
+        final.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    } else if (score2 > score1) {
+
+        final.push(equipo2)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo2}`)
+    } else if (score1 === score2) {
+
+        final.push(equipo1)
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    }
+}
+function jugartercerYcuarto(equipo1, score1, equipo2, score2) {
+
+    if (score1 > score2) {
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    } else if (score2 > score1) {
+
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo2}`)
+    } else if (score1 === score2) {
+
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    }
+}
+function jugarfinal(equipo1, score1, equipo2, score2) {
+
+    if (score1 > score2) {
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    } else if (score2 > score1) {
+
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo2}`)
+    } else if (score1 === score2) {
+
+
+        return console.log(`${equipo1} ${score1} vs ${equipo2} ${score2} => ${equipo1}`)
+    }
+}
+console.log(`================================================`)
+console.log(`======COMIENZO DE LA FASE DE ELIMINATORIAS======`)
+console.log(`================================================`)
+console.log(`PLAY OFF`)
+faseEliminatorias.forEach(iterador => {
+    console.log(iterador)
+});
+console.log("===OCTAVOS DE FINAL===")
+jugarOctavos(grupoA.equipo1, generaScore1(), grupoB.equipo2, generaScore2())
+jugarOctavos(grupoA.equipo2, generaScore1(), grupoB.equipo1, generaScore2())
+jugarOctavos(grupoC.equipo1, generaScore1(), grupoD.equipo2, generaScore2())
+jugarOctavos(grupoC.equipo2, generaScore1(), grupoD.equipo1, generaScore2())
+jugarOctavos(grupoE.equipo1, generaScore1(), grupoF.equipo2, generaScore2())
+jugarOctavos(grupoE.equipo2, generaScore1(), grupoF.equipo1, generaScore2())
+jugarOctavos(grupoG.equipo1, generaScore1(), grupoH.equipo2, generaScore2())
+jugarOctavos(grupoG.equipo2, generaScore1(), grupoH.equipo1, generaScore2())
+
+console.log(`===CUARTOS DE FINAL===`)
+jugar4tos(cuartosDeFinal[0], generaScore1(), cuartosDeFinal[1], generaScore2())
+jugar4tos(cuartosDeFinal[2], generaScore1(), cuartosDeFinal[3], generaScore2())
+jugar4tos(cuartosDeFinal[4], generaScore1(), cuartosDeFinal[5], generaScore2())
+jugar4tos(cuartosDeFinal[6], generaScore1(), cuartosDeFinal[7], generaScore2())
+
+console.log(`===SEMIFINALES===`)
+jugarsemifinal(semiFinales[0], generaScore1(), semiFinales[1], generaScore2())
+jugarsemifinal(semiFinales[2], generaScore1(), semiFinales[3], generaScore2())
+console.log(`===TERCER Y CUARTO PUESTO===`)
+jugarfinal(semiFinales[0], generaScore1(), semiFinales[3], generaScore2())
+console.log(`===FINAL===`)
+jugarfinal(final[0], generaScore1(), final[1], generaScore2())
+
+// final.forEach(i => {
+//     console.log(i)
+// });
+
+
+
